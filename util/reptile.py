3bda3264ee7791f1582cf67b3c963046f9ada4aa
肖恒 <xiaoheng03@meituan.com>
2021-01-24 22:58:42 +0800

    封装类

--- util/reptile.py
+++ util/reptile.py
@@ -3,22 +3,27 @@ import json
 
 class  Reptile():
     def getAppstoreUserCommentData(self,appId):
-        appIdUrl="https://itunes.apple.com/CN/rss/customerreviews/page=1/id="+appId+"/sortby=mostrecent/json?l=en&cc=cn"
+        page_num=1
+        appIdUrl="https://itunes.apple.com/CN/rss/customerreviews/page="+page_num+"/id="+appId+"/sortby=mostrecent/json?l=en&cc=cn"
+        print("获取第"+page_num+"页数据")
         print("appIdUrl:"+appIdUrl)
-        response=requests.get(appIdUrl)
-        data=response.text
-        data_json=json.dumps(data)
-        print("data_json:" + data_json)
-        data_updated_json=data_json['feed']['updated']
-        data_entry_json=data_json['feed']['entry']
+        response=requests.request("GET",appIdUrl)
+        # data=response.text
+        data_dict=json.loads(response.content.decode('utf8'))
+        # print("data_json:" + data_json)
+        data_updated=data_dict['feed']['updated']
+        print("评论更新时间: "+data_updated)
+        data_entry_json=data_dict['feed']['entry']
         data_entry_json_len=len(data_entry_json)
-        print("评论data_entry_json_len共有:"+data_entry_json_len+"个")
+        print("评论data_entry_json_len共有:"+str(data_entry_json_len)+"个")
         for i in range (0,data_entry_json_len):
             rating=data_entry_json[i]['im:rating']
             print("rating:",rating)
-            content=data_entry_json['content']
-            print("content")
-            version=data_entry_json['im:version']
-            user_name=data_entry_json['author']['name']
+            content=data_entry_json[i]['content']
+            print("content:",content)
+            version=data_entry_json[i]['im:version']
+            print("version:",version)
+            user_name=data_entry_json[i]['author']['name']
+            print("user_name:",user_name)
 
 
