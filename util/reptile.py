d8d599f21b5bebd1b5cea29baa55948af4806cd6
肖恒 <xiaoheng03@meituan.com>
2021-01-24 23:01:50 +0800

    封装类

--- util/reptile.py
+++ util/reptile.py
@@ -3,16 +3,16 @@ import json
 
 class  Reptile():
     def getAppstoreUserCommentData(self,appId):
-        page_num=1
-        appIdUrl="https://itunes.apple.com/CN/rss/customerreviews/page="+page_num+"/id="+appId+"/sortby=mostrecent/json?l=en&cc=cn"
-        print("获取第"+page_num+"页数据")
+        page_num=3
+        appIdUrl="https://itunes.apple.com/CN/rss/customerreviews/page="+str(page_num)+"/id="+str(appId)+"/sortby=mostrecent/json?l=en&cc=cn"
+        print("获取第"+str(page_num)+"页数据")
         print("appIdUrl:"+appIdUrl)
         response=requests.request("GET",appIdUrl)
         # data=response.text
         data_dict=json.loads(response.content.decode('utf8'))
         # print("data_json:" + data_json)
         data_updated=data_dict['feed']['updated']
-        print("评论更新时间: "+data_updated)
+        print("评论更新时间: "+str(data_updated))
         data_entry_json=data_dict['feed']['entry']
         data_entry_json_len=len(data_entry_json)
         print("评论data_entry_json_len共有:"+str(data_entry_json_len)+"个")
